3168d1fb06544acab918054ebf3a3b6dcaf46908
sravani <chsravani063@gmail.com>
2024-04-09 21:56:11 +0530

    design modified

--- src/app/component/Featurecard/Featurecard.js
+++ src/app/component/Featurecard/Featurecard.js
@@ -1,25 +1,24 @@
 import styles from "./featurecard.module.css"
 export default function Featurecard(props) {
     return (
-        <div className={styles.main_container}>
-            <div className={styles.container}>
-                <div><h1 className={styles.header1}>{props.title}</h1></div>
-                <div>
-                    <p className={styles.paragraph_text}>{props.description}</p>
+        <div className={styles.header}>
+            <div className={styles.c}>
+                <div ><h1 className={styles.header1}>{props.title}</h1></div>
+                <div className={styles.paragraph_text}>
+                    <p >{props.description}</p>
                 </div>
             </div>
-            <div className={styles.img_container}>
+            <div className={styles.container2}>
                 <div >
                     <img src="tvimage.png" className={styles.img}></img>
                 </div>
-                <div className={styles.video}>
-                    <video width="60%" height="240" autoPlay loop>
+                <div className={styles.videostyle}>
+                    <video width="100%" height="100%" autoPlay loop>
                         <source src="video-tv-in-0819.mp4" type="video/mp4" />
                     </video>
                 </div>
             </div>
+
         </div>
-
-
     );
 }
